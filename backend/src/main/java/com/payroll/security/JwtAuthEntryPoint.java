04d2e4bd1f6a03cdca85d4e8812c8110b5d4482a
Anurag Kumar <142084822+Anuragkumar103@users.noreply.github.com>
2026-07-11 13:01:19 +0530

    Update JwtAuthEntryPoint.java

--- backend/src/main/java/com/payroll/security/JwtAuthEntryPoint.java
+++ backend/src/main/java/com/payroll/security/JwtAuthEntryPoint.java
@@ -10,7 +10,6 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-
 @Component
 public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
 
